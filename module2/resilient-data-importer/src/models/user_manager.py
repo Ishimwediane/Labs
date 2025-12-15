202e8f954ae7463b8f63ea88cb8f15e2cdba2237
Ishimwediane <ishimwediane400@gmail.com>
2025-12-15 11:02:53 +0200

    add model user and user_manager

--- module2/resilient-data-importer/src/models/user_manager.py
+++ module2/resilient-data-importer/src/models/user_manager.py
@@ -1,5 +1,5 @@
 import csv
-from models.user import User
+from .user import User
 
 
 class UserManager:
@@ -8,13 +8,15 @@ class UserManager:
         self.users = []
 
     def load_users(self):
-        """Load users from the CSV file and save then as User object"""
+        """Load users from the CSV file and store them as User objects."""
         try:
             with open(self.filename, newline="", encoding="utf-8") as f:
                 reader = csv.DictReader(f)
                 for row in reader:
                     user = User(
-                        name=row["name"], age=int(row["age"]), email=row["email"]
+                        user_id=int(row["user_id"]),  # convert string to int
+                        name=row["name"],  # keep as string
+                        email=row["email"],
                     )
                     self.users.append(user)
             print(f"Loaded {len(self.users)} users from {self.filename}")
@@ -22,27 +24,31 @@ class UserManager:
             print(f"File not found: {self.filename}")
 
     def save_users(self):
-        """Save users to the CSV file"""
+        """Save users to the CSV file."""
         try:
             with open(self.filename, "w", newline="", encoding="utf-8") as f:
-                fieldnames = ["name", "age", "email"]
+                fieldnames = ["user_id", "name", "email"]
                 writer = csv.DictWriter(f, fieldnames=fieldnames)
                 writer.writeheader()
                 for user in self.users:
                     writer.writerow(
-                        {"name": user.name, "age": user.age, "email": user.email}
+                        {
+                            "user_id": user.user_id,
+                            "name": user.name,
+                            "email": user.email,
+                        }
                     )
-                print(f"Saved {len(self.users)} users to {self.filename}")
+            print(f"Saved {len(self.users)} users to {self.filename}")
         except Exception as e:
             print(f"Error saving users: {e}")
 
     def add_user(self, user: User):
-        """Add a new user to the list"""
+        """Add a new user to the list."""
         self.users.append(user)
         print(f"Added user: {user}")
 
     def find_user(self, email: str):
-        """Find a user by email"""
+        """Find a user by email."""
         for user in self.users:
             if user.email == email:
                 return user
