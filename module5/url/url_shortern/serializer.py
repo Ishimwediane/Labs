8aa29290a9968dd5cdb59944936700baf4b50211
Ishimwediane <ishimwediane400@gmail.com>
2026-01-26 11:10:31 +0200

    redirect url

--- module5/url/url_shortern/serializer.py
+++ module5/url/url_shortern/serializer.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from .models import Url
 
-class UrlSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Url
-        fields = ['original_url', 'short_url', 'created_at']
-        read_only_fields = ['short_url', 'created_at'] 
-        
+
+class UrlCreateSerializer(serializers.Serializer):
+    original_url = serializers.URLField(max_length=2000)
+    
     def validate_original_url(self, value):
         if not value.startswith(('http://', 'https://')):
             raise serializers.ValidationError("URL must start with http:// or https://")
         return value
+
+
+class UrlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Url
+        fields = ['id', 'original_url', 'short_url', 'created_at']
+        read_only_fields = ['id', 'short_url', 'created_at']
+
+
