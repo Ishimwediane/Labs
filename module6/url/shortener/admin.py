773e428e65bf2d7e6c031ba07119f0001540bb8b
Ishimwediane <ishimwediane400@gmail.com>
2026-02-10 12:17:11 +0200

    testing

--- module6/url/shortener/admin.py
+++ module6/url/shortener/admin.py
@@ -3,6 +3,6 @@ from .models import Url
 
 @admin.register(Url)
 class UrlAdmin(admin.ModelAdmin):
-    list_display = ('original_url', 'short_url', 'created_at')
-    search_fields = ('original_url', 'short_url')
-    ordering = ('-created_at',)
+    list_display = ('original_url', 'short_url', 'owner', 'click_count', 'is_active', 'expires_at')
+    search_fields = ('original_url', 'short_url', 'owner__username')
+    readonly_fields = ('short_url', 'click_count')  
--- module6/url/url/settings.py
+++ module6/url/url/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from decouple import config, Csv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,10 +83,15 @@ WSGI_APPLICATION = "url.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": "db",  # Docker service name
+        "PORT": 5432,
     }
 }
+
 
 
 # Password validation
