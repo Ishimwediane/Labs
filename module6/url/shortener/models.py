4feff6237e1c44ec90f61c9edd0e10d374bd82d0
Ishimwediane <ishimwediane400@gmail.com>
2026-02-10 09:49:43 +0200

    adding queryset

--- module6/url/shortener/models.py
+++ module6/url/shortener/models.py
@@ -1,9 +1,43 @@
 from django.db import models
+from django.conf import settings
+from .managers import URLQuerySet
+
+
+class Tag(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Url(models.Model):
     original_url = models.URLField(max_length=2000)
     short_url = models.CharField(max_length=10, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    owner=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='urls')
+    click_count = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    expires_at=models.DateTimeField(null=True,blank=True)
+    tags=models.ManyToManyField(Tag,related_name='urls',blank=True)
+    
+    #query set
+    objects=URLQuerySet.as_manager()
+    
     def __str__(self):
-        return self.original_url
+        return self.short_url
+
+class Click(models.Model):
+    url=models.ForeignKey(Url,on_delete=models.CASCADE,related_name='clicks')
+    ip_address=models.GenericIPAddressField()
+    user_agent=models.TextField()
+    country=models.CharField(max_length=100,blank=True,null=True)
+    city=models.CharField(max_length=100,blank=True,null=True)
+    referer=models.URLField(max_length=2000,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.ip_address} - {self.url.short_url}"
+
+
+
+    
+
+
--- module6/url/url/settings.py
+++ module6/url/url/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "core",
     "shortener",
     "api",
-    "accounts"
+    "accounts",
     "rest_framework",
     "drf_spectacular",
 ]
