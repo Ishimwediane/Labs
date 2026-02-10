24b98d8d73628a8dfd4279c6040167c9b1ec5ad4
Ishimwediane <ishimwediane400@gmail.com>
2026-02-10 16:11:50 +0200

    optimization of lab

---
+++ module4/Lab1_Ecommerce/src/utils/logger.py
@@ -0,0 +1,39 @@
+
+def log_info(message: str):
+    """
+    Log an informational message.
+
+    """
+    print(f"[INFO] {message}")
+
+
+def log_success(message: str):
+    """
+    Log a success message.
+
+    """
+    print(f"[SUCCESS] {message}")
+
+
+def log_error(message: str):
+    """
+    Log an error message.
+
+    """
+    print(f"[ERROR] {message}")
+
+
+def log_warning(message: str):
+    """
+    Log a warning message.
+
+    """
+    print(f"[WARNING] {message}")
+
+
+def log_debug(message: str):
+    """
+    Log a debug message.
+
+    """
+    print(f"[DEBUG] {message}")
