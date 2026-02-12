7ad5c0584a4ad042ce26034f20748289814b43a1
Ishimwediane <ishimwediane400@gmail.com>
2026-02-12 12:02:12 +0200

    adding permission

--- module7/url/shortener/views.py
+++ module7/url/shortener/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from url.shortener.models import Url
+from .models import Url
+from api.serializers import UrlSerializer, UrlCreateSerializer 
+from rest_framework.permissions import IsAuthenticated 
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+class UrlViewSet(ModelViewSet):
+    queryset = Url.objects.all()
+    serializer_class = UrlSerializer
+    permission_classes = [IsAuthenticated,IsOwnerOrReadOnly]
