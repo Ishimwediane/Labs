824a9e25c3fc61745fbfc9d0df2c858d8993188a
Ishimwediane <ishimwediane400@gmail.com>
2026-02-12 11:03:38 +0200

    login view

--- module7/url/accounts/urls.py
+++ module7/url/accounts/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterAPIView
+from .views import RegisterAPIView, LoginAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
