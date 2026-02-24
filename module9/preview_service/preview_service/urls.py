792656c6e76b54ba25def877249523ba75c07a19
Ishimwediane <ishimwediane400@gmail.com>
2026-02-24 05:50:09 +0200

    fix(preview): add missing SpectacularAPIView to fix Swagger UI NoReverseMatch

--- module9/preview_service/preview_service/urls.py
+++ module9/preview_service/preview_service/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from preview.views import PreviewView
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("fetch-preview/", PreviewView.as_view(), name="fetch_preview"),
     
     # Swagger/OpenAPI Documentation for Preview Service
-    
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
