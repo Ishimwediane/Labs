0a552a304ce9cc247987af348340e5b32ec92802
Ishimwediane <ishimwediane400@gmail.com>
2026-03-03 10:23:20 +0200

    feat(db): add model-level validations for data integrity

--- module9/url/accounts/models.py
+++ module9/url/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 class UserTier(models.TextChoices):
     FREE="free", "Free"
@@ -8,8 +9,14 @@ class UserTier(models.TextChoices):
     
 class User(AbstractUser):
     is_premium = models.BooleanField(default=False)
-
     tier = models.CharField(max_length=20,choices=UserTier.choices,default=UserTier.FREE)
+
+    def clean(self):
+        super().clean()
+        if self.is_premium and self.tier == UserTier.FREE:
+            raise ValidationError("A premium user cannot have a 'free' tier.")
+        if not self.is_premium and self.tier != UserTier.FREE:
+            raise ValidationError("A non-premium user must be on the 'free' tier.")
 
     def __str__(self):
         return self.username
